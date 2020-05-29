7c077237780ac9d6489c6125092a0eaf0592b52b
nyakundigeorge <dev.things.ke@gmail.com>
2020-05-29 17:18:47 +0300

    feat(server): add a simple server to return hello world and ok status

--- src/main.go
+++ src/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+// ServeHTTP method on the server struct is implementing the Handler interface
+// we're setting the content type to application/json and the return a ok status
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
 
 func main() {
- fmt.Println("Say My Name")
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
